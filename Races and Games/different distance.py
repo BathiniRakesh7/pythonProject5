c10a60b8a43165742af8675f3c5e8023c714d31a
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-03 10:28:59 +0530

    races

---
+++ Races and Games/different distance.py
@@ -0,0 +1,11 @@
+
+A_beat_B = int(input("A beat B="))
+B_beat_C = int(input("B beat C="))
+A = int(input("Winner distance of A= "))
+B = A - A_beat_B
+b = int(input("Winning distance of b="))
+C = b - B_beat_C
+distance = (A / B * b / C)
+a_beat_c = int(input("A beat C in how much distance"))
+A_beat_C = a_beat_c - (a_beat_c / distance)
+print("A_beat_C ="+ str(A_beat_C))

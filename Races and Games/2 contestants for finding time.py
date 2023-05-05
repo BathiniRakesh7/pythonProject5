8508437476a6ee17220ba06d32bd42738998d6c1
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-05 12:55:38 +0530

    races

--- Races and Games/2 contestants for finding time.py
+++ Races and Games/2 contestants for finding time.py
@@ -1,12 +1,12 @@
 condition = input("1)two contestants or 2)three contestants =")
 
-if condition=="1":
+if condition == "1":
     race_distance = float(input("Enter the race distance in meters="))
     A_beat_B_distance = float(input("Enter A beats by B distance in meters="))
     A_beat_B_time = float(input("Enter A beats by B time in sec="))
     time_taken_B = (A_beat_B_time/A_beat_B_distance*race_distance)
     print("Time taken by B ="+str(time_taken_B)+"sec")
-    time_taken_A=(time_taken_B - A_beat_B_time)
+    time_taken_A = (time_taken_B - A_beat_B_time)
     print("Time taken by A ="+str(time_taken_A)+"sec")
 
 else:
@@ -16,9 +16,9 @@ else:
     A_beat_C = float(input("Enter A beats C ="))
     time_taken_C = (A_beat_B_time + B_beat_C_time)
     print("Time taken by C ="+str(time_taken_C)+"sec")
-    total_time_taken_C=(time_taken_C/A_beat_C*race_distance)
+    total_time_taken_C = (time_taken_C/A_beat_C*race_distance)
     print("Time taken by C ="+str(time_taken_C)+"sec")
-    time_taken_A=(total_time_taken_C-time_taken_C)
+    time_taken_A = (total_time_taken_C-time_taken_C)
     print("Time taken by A ="+str(time_taken_A)+"sec")
 
 

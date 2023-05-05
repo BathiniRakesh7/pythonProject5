7777d0232f4db6d26da88d04380b390048579d4a
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-05 16:40:20 +0530

    races

--- Races and Games/2 contestants to find distance by time.py
+++ Races and Games/2 contestants to find distance by time.py
@@ -1,11 +1,13 @@
 def calculating_A_beats_by_B(time_taken_by_A, time_taken_by_B, race_distance, time_difference):
     A_beats_by_B = (race_distance / time_taken_by_B * time_difference)
-    print("A_beat_B ="+str(A_beats_by_B))
+    print(f'A beat B = {race_distance}/{time_taken_by_B}*{time_difference}')
+    print("A_beat_B ="+str(A_beats_by_B) + 'm')
 
 
 def calculating_B_beats_by_A(time_taken_by_A, time_taken_by_B, race_distance, time_difference):
     B_beats_by_A = (race_distance / time_taken_by_A * time_difference)
-    print("B_beat_A ="+str(B_beats_by_A))
+    print(f'B beat A = {race_distance}/{time_taken_by_A}*{time_difference}')
+    print("B_beat_A ="+str(B_beats_by_A) + 'm')
 
 condition = input('a)A_beats_by_B or b)B_beats_by_A =').lower()
 race_distance = int(input("enter race distance = "))
--- Races and Games/finding distance by speed ratios.py
+++ Races and Games/finding distance by speed ratios.py
@@ -16,10 +16,13 @@ else:
     A_is_start_of_B = float(input("A has start of B ="))
 
     distance_covered_A = (race_distance - A_is_start_of_B)
+    print(f'distance covered by A = {race_distance}-{A_is_start_of_B}')
     print("distance covered by A =" + str(distance_covered_A))
 
     distance_covered_B = (B_speed_ratio/A_speed_ratio*distance_covered_A)
+    print(f'distance covered by B = {B_speed_ratio}/{A_speed_ratio}*{distance_covered_A}')
     print("distance covered by B =" + str(distance_covered_B))
 
     A_wins_by = (race_distance - distance_covered_B)
+    print(f'A wins by = {race_distance}-{distance_covered_B}')
     print("A wins by =" + str(A_wins_by) + "m")

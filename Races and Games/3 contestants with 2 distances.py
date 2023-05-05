d0ca97b5ef20c1acf415fe918efe4aaa2a770a72
Rakesh Bathini <rakeshbathini07@gmail.com>
2023-05-05 11:19:44 +0530

    races

--- Races and Games/3 contestants with 2 distances.py
+++ Races and Games/3 contestants with 2 distances.py
@@ -1,6 +1,5 @@
 
-
-condition= input("1)B_beat_C or 2)C_beat_B=")
+condition=input("1)B_beat_C or 2)C_beat_B=")
 
 if condition=="1":
     A_beat_B = float(input("A beat B="))
@@ -8,7 +7,7 @@ if condition=="1":
     A = float(input("Winner distance of A= "))
     B = A - A_beat_B
     C = A - A_beat_C
-    distance = (B/ A * A/ C)
+    distance = (B/A * A/C)
     b_beat_c = float(input("B beat C in how much distance="))
     B_beat_C = b_beat_c - (b_beat_c / distance)
     print("B_beat_C =" + str(B_beat_C))
@@ -18,7 +17,7 @@ else:
     A = float(input("Winner distance of A= "))
     B = A - A_beat_B
     C = A - A_beat_C
-    distance = (C/ A * A/ B)
+    distance = (C/A * A/B)
     c_beat_b = float(input("C beat B in how much distance="))
     C_beat_B = c_beat_b - (c_beat_b / distance)
     print("C_beat_B =" + str(C_beat_B))
